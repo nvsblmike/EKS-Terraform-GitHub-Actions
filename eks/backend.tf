18930f48d787d8619e6ce312991867fea40c3b24
Alese Michael Toluwani <91319060+nvsblmike@users.noreply.github.com>
2025-06-19 16:23:29 +0100

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,10 +7,10 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "dev-aman-tf-bucket"
+    bucket         = "terraform-eks-1925"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    dynamodb_table = "terraform-lock-table"
     encrypt        = true
   }
 }
