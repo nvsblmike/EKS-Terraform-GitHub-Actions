e232f785c619982ac5af6211599a0f55e6bf8983
Alese Michael Toluwani <91319060+nvsblmike@users.noreply.github.com>
2025-06-19 16:32:21 +0100

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -1,11 +1,13 @@
 terraform {
-  required_version = "~> 1.9.3"
+  required_version = ">= 1.5.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.49.0"
+      version = "~> 5.0"
     }
   }
+}
+
   backend "s3" {
     bucket         = "terraform-eks-1925"
     region         = "us-east-1"
